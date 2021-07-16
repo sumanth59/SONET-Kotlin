efc1df87c40af0da81e9413e20bae114b2ede7a5
sumanth59 <kasam.sumanth59@gmail.com>
2021-07-16 21:27:20 +0530

    Person Details

---
+++ app/src/main/java/com/example/kotlindemo/MainActivity6.kt
@@ -0,0 +1,32 @@
+package com.example.kotlindemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main6)
+        var name = findViewById<EditText>(R.id.et_6_1)
+        var gender = findViewById<EditText>(R.id.et_6_2)
+        var age = findViewById<EditText>(R.id.et_6_3)
+        var btn = findViewById<Button>(R.id.btn_6)
+
+        btn.setOnClickListener(View.OnClickListener {
+            val person = person()
+            person.name = name.text.toString()
+            person.gender = gender.text.toString()
+            person.age = age.text.toString().toInt()
+
+             var result = if(person.age!=0)"${person.name}" else "Person is minor"
+
+             Toast.makeText(this, result, Toast.LENGTH_LONG).show()
+
+
+        });
+    }
+}
